74022f8d184bd006653762281470adb3ff6ff1cb
Your Name <you@example.com>
2021-01-03 17:14:18 +0800

    install laravel-admin

--- database/migrations/2020_12_27_070445_create_user_profile_table.php
+++ database/migrations/2020_12_27_070445_create_user_profile_table.php
@@ -17,7 +17,7 @@ class CreateUserProfileTable extends Migration
             $table->id();
             $table->integer('user_id')->comment('用户编码');
             $table->string('nick_name')->comment('用户昵称');
-            $table->tinyInteger('age',3)->comment('年龄');
+            $table->tinyInteger('age')->comment('年龄');
             $table->string('name')->comment('用户真实姓名');
             $table->enum('sex',[0,1,2])->default(0)->comment('0 保密 1:男 2:女');
             $table->integer('city_id')->comment('城市编码'); // city
