3527ec7da3f7069a1b87c0f2ecb4024a366b0995
Your Name <you@example.com>
2021-01-03 18:50:44 +0800

    update route local

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-    $api->get('register', 'UserController@register');
+    // 用户注册
+    $api->post('register','UserController@register');
+    // 用户登录
+    $api->post('login','UserController@login');
+
+    // 用户收获地址的相关接口
+    $api->get('user/address/{addressId}','UserAddressController@profile');
+    // 获取用户收货地址列表
+    $api->get('user/address','UserAddressController@list');
+    // 对用户收货地址进行更改
+    $api->put('user/address','UserAddressController@update');
+    // 创建用户收货地址
+    $api->post('user/address','UserAddressController@create');
+
+    // 用户银行卡的相关接口
+    $api->get('user/card/{addressId}','UserCardController@profile');
+    // 获取用户银行卡列表
+    $api->get('user/card','UserCardController@list');
+    // 对用户银行卡进行更改
+    $api->put('user/card','UserCardController@update');
+    // 创建用户银行卡
+    $api->post('user/card','UserCardController@create');
 });
